fe3cd25420770940b20fa95398a71a6f10569be1
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-04-14 10:01:39 -0700

    simple solution to dependency problem

--- remote_remote_dependencies.ts
+++ remote_remote_dependencies.ts
@@ -1,7 +1,6 @@
 // Deno has verified third party modules for safety
 
 // deno info <file>, to get more info on the file and get a dependency graph
-
 import { path } from "./deps.ts";
 
 console.log(path.basename("/folder/folder/folder/file.txt"));
