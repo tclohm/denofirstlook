2d8e92589e2f91004b03baae7fbd1992b61afdef
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-04-14 13:24:38 -0700

    oak

--- deps.ts
+++ deps.ts
@@ -1,4 +1,5 @@
 // put all your dependencies here
 import * as path from "https://deno.land/std@0.73.0/path/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 
-export { path }
+export { path, Application, Router }
