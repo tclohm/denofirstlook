e5daa8d8fc3a25afeea542e7f27313f2d94f0ba1
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-04-14 09:34:05 -0700

    CLI

--- app.ts
+++ app.ts
@@ -1,8 +1,8 @@
-const message: string = "Hello World"
+const message: string = "Hello World";
 
 console.log(message);
 
 // MARK: -- make module
 // MARK: -- deno run app.ts
 // MARK: -- deno run --no-check app.ts
-export { };
+export {};
