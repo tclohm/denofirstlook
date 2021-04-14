457f687878c1445286dd0e41059445997d7e04d7
Taylor Lohman <taylor@Taylors-MacBook-Pro.local>
2021-04-14 10:13:37 -0700

    permissions and asking the user

--- reading.ts
+++ reading.ts
@@ -1,7 +1,21 @@
-const results = await Deno.readDir('../');
+// request permissions
+const desc = { name: "read", path: "../" } as const;
 
-for await (const result of results) {
-	console.log(result);
+const status = await Deno.permissions.request(desc);
+
+if (status.state === "granted") {
+
+	const results = await Deno.readDir('../');
+
+	for await (const result of results) {
+		console.log(result);
+	}
+
+} else {
+	console.log("This program needs read permission to run")
 }
 
 // deno run --allow-read <file>
+// deno run --unstable <file> , when dealing with permissions
+
+export {};
